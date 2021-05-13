c11f216b447ec1398dd3ff2ebe670ec916f4ed54
Slaninecka <56261015+Slaninecka@users.noreply.github.com>
2021-05-13 16:49:18 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -8,9 +8,9 @@ import subprocess
 
 # Input injection
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+    command = 'ffmpeg -i "{source}" output_file.mpg'
+    .format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
-
 
 # Assert statements
 def foo(request, user):
